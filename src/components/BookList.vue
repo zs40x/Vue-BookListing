dc640bf1e604b1219c64150bd6444604265a0696
Stefan Mehnert <s.mehnert@polipol.de>
2019-08-20 15:36:21 +0200

    Added a list of books with style and static content.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
     <h1>{{title}}</h1>
+    <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+    </ul>
 </div>    
 </template>
 
@@ -18,5 +23,13 @@ export default {
 <style>
 h1, h2 {
     font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
 }
 </style>
