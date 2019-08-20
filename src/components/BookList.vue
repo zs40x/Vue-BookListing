d193098dca9cd092591b496c24d3ea7f5e8d6412
Stefan Mehnert <s.mehnert@polipol.de>
2019-08-20 15:30:54 +0200

    New Book-List compoment.

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>{{title}}</h1>
+</div>    
+</template>
+
+<script>
+export default {
+    name: 'Booklist',
+    data() {
+        return {
+            title: 'All Books'
+        }
+    }
+}
+</script>
+
+<style>
+h1, h2 {
+    font-weight: normal;
+}
+</style>
