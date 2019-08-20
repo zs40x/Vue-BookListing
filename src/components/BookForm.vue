33e522b9eee566794da755c878e37de290248ec0
Stefan Mehnert <s.mehnert@polipol.de>
2019-08-20 16:03:27 +0200

    Added new bookForm component.

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" method="POST">
+      <input v-model="bookTitle" type="text" name="title" placeholder="Book Title"/>
+      <input v-model="bookAuthor" type="text" name="author" placeholder="Book Author"/>
+      <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data() {
+        return {
+            bookTitle: '',
+            bookAuthor: ''
+        }
+    },
+    methods: {
+        bookSubmit(bookTitle, bookAuthor) {
+            this.$emit('addBook', bookTitle, bookAuthor);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
